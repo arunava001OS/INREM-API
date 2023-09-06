5c4174eeb3df0d598772a197dda3f1692dbbb259
arunava001OS <arunavasarkar3@gmail.com>
2023-09-06 18:25:09 +0530

    renamed files route -> server , server -> route

--- route.js
+++ route.js
@@ -1,89 +1,24 @@
-const {User,Course} = require('./model');
 
-const {db} = require('./firebase');
+const express = require('express');
+const app = express();
 
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
 
-const SECRET_KEY = "INREMAPI";
-const TOKEN_EXPIRY = "3d";
+const {signup,login,getAllCourses,upsertCourse} = require('./server'); 
+const {AuthMiddleWare} = require('./authMiddleware')
 
-const createToken = (obj)=>{
-    return jwt.sign({obj},SECRET_KEY, {expiresIn: TOKEN_EXPIRY});
-}
+/// Endpoints
 
-const signup = async (req,res) => {
-    var userObj = new User(req.body);
-    try{
-        //encrypt password
-        const hashedPass = await bcrypt.hash(userObj.password,10);
-        userObj.password = hashedPass;
-        const token = createToken(userObj.userID);
-        const userDB = db.collection('User');
-        var userData = await userDB.doc(userObj.userID).get();
-        if(userData.exists) {
-            res.status(400).send("User is already present. Login");
-            return;
-        } else {
-            var userData = await userDB.doc(userObj.userID).set(JSON.parse(JSON.stringify(userObj)));
-            res.cookie('jwt',token, {httpOnly:true});
-            res.status(200).send({
-                "message": "User Created Successfully"
-            });
-        }
-    }catch(e){
-        res.status(400).send(e);
-    }
-}
+app.get('/',async (req,res) => {
+    res.json("Welcome to INREM API");
+})
 
-const login = async (req,res) => {
-    const {userID,password} = req.body;
+app.post('/signup', (req,res) => signup(req,res));
+app.post('/login',(req,res)=> login(req,res));
 
-    try{
-        const userDB = db.collection('User');
-        var userData = await userDB.doc(userID).get()
-        if(userData.exists){
-            const matchpassword = await bcrypt.compare(password,userData.data().password);
-            if(matchpassword){
-                const token = createToken(userID);
-                //const token = jwt.sign({id:userID},SECRET_KEY,{expiresIn: TOKEN_EXPIRY});
-                res.cookie('jwt',token, {httpOnly:true});
-                res.status(200).send({
-                    "message": "User logged in successfully"
-                })
-            }else{
-                res.status(400).send("Incorrect Credentials");
-                return;
-            }
-        }else{
-            res.status(400).send("User does not exist. Signup First");
-            return;
-        }
-    }catch(e){
-        res.status(400).send(e);
-    }
-}
-
-const getAllCourses = async (req,res) => {
-    var courseDB = db.collection("Course");
-    var courses = await courseDB.get();
-    var courseArray = [];
-    courses.forEach((doc) => {
-        courseArray.push(new Course(doc.data()));
-    })
-    res.status(200).send(courseArray.slice(0,-1)); // returning empty obj at end not sure why ?
-}
-
-const upsertCourse = async (req,res) => {
-    var courseObj = new Course(req.body);
-
-    var courseDB = db.collection('Course');
-    await courseDB.doc(courseObj.courseID).set(JSON.parse(JSON.stringify(courseObj)));
-    res.status(200).send({
-        "message": "Course Created/Updated Successfully"
-    })
-
-}
+app.get('/course', (req,res) => getAllCourses(req,res));
+app.post('/course',AuthMiddleWare(["course-admin","admin"]),(req,res) => upsertCourse(req,res))
 
 
-module.exports = {signup,login,getAllCourses,upsertCourse}
+app.listen(3000);
--- server.js
+++ server.js
@@ -1,24 +1,89 @@
+const {User,Course} = require('./model');
 
-const express = require('express');
-const app = express();
+const {db} = require('./firebase');
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
-const {signup,login,getAllCourses,upsertCourse} = require('./route'); 
-const {AuthMiddleWare} = require('./authMiddleware')
+const SECRET_KEY = "INREMAPI";
+const TOKEN_EXPIRY = "3d";
 
-/// Endpoints
+const createToken = (obj)=>{
+    return jwt.sign({obj},SECRET_KEY, {expiresIn: TOKEN_EXPIRY});
+}
 
-app.get('/',async (req,res) => {
-    res.json("Welcome to INREM API");
-})
+const signup = async (req,res) => {
+    var userObj = new User(req.body);
+    try{
+        //encrypt password
+        const hashedPass = await bcrypt.hash(userObj.password,10);
+        userObj.password = hashedPass;
+        const token = createToken(userObj.userID);
+        const userDB = db.collection('User');
+        var userData = await userDB.doc(userObj.userID).get();
+        if(userData.exists) {
+            res.status(400).send("User is already present. Login");
+            return;
+        } else {
+            var userData = await userDB.doc(userObj.userID).set(JSON.parse(JSON.stringify(userObj)));
+            res.cookie('jwt',token, {httpOnly:true});
+            res.status(200).send({
+                "message": "User Created Successfully"
+            });
+        }
+    }catch(e){
+        res.status(400).send(e);
+    }
+}
 
-app.post('/signup', (req,res) => signup(req,res));
-app.post('/login',(req,res)=> login(req,res));
+const login = async (req,res) => {
+    const {userID,password} = req.body;
 
-app.get('/course', (req,res) => getAllCourses(req,res));
-app.post('/course',AuthMiddleWare(["course-admin","admin"]),(req,res) => upsertCourse(req,res))
+    try{
+        const userDB = db.collection('User');
+        var userData = await userDB.doc(userID).get()
+        if(userData.exists){
+            const matchpassword = await bcrypt.compare(password,userData.data().password);
+            if(matchpassword){
+                const token = createToken(userID);
+                //const token = jwt.sign({id:userID},SECRET_KEY,{expiresIn: TOKEN_EXPIRY});
+                res.cookie('jwt',token, {httpOnly:true});
+                res.status(200).send({
+                    "message": "User logged in successfully"
+                })
+            }else{
+                res.status(400).send("Incorrect Credentials");
+                return;
+            }
+        }else{
+            res.status(400).send("User does not exist. Signup First");
+            return;
+        }
+    }catch(e){
+        res.status(400).send(e);
+    }
+}
+
+const getAllCourses = async (req,res) => {
+    var courseDB = db.collection("Course");
+    var courses = await courseDB.get();
+    var courseArray = [];
+    courses.forEach((doc) => {
+        courseArray.push(new Course(doc.data()));
+    })
+    res.status(200).send(courseArray.slice(0,-1)); // returning empty obj at end not sure why ?
+}
+
+const upsertCourse = async (req,res) => {
+    var courseObj = new Course(req.body);
+
+    var courseDB = db.collection('Course');
+    await courseDB.doc(courseObj.courseID).set(JSON.parse(JSON.stringify(courseObj)));
+    res.status(200).send({
+        "message": "Course Created/Updated Successfully"
+    })
+
+}
 
 
-app.listen(3000);
+module.exports = {signup,login,getAllCourses,upsertCourse}
